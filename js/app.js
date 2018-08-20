b7fd8a43d8a950b9be243bd242cf664e40872948
Sara-Seibt <sara.seibt@gmail.com>
2018-08-20 14:08:04 -0500

    type: style
    updated names to icons array, previous incorrect

--- js/app.js
+++ js/app.js
@@ -1,17 +1,32 @@
-/* Array that holds the cards
-const cardIcon =
-['fa-diamond', 'fa-diamond',
-'fa-paper-plane-o', 'fa-paper-plane-o',
-'fa-bolt', 'fa-bolt',
-'fa-anchor', 'fa-anchor',
-'fa-cube', 'fa-cube',
-'fa-leaf', 'fa-leaf',
-'fa-bomb', 'fa-bomb',
-'fa-bicycle', 'fa-bicycle'];
-*/
+ const icons =
+['fa fa-diamond', 'fa fa-diamond',
+'fa fa-paper-plane-o', 'fa fa-paper-plane-o',
+'fa fa-bolt', 'fa fa-bolt',
+'fa fa-anchor', 'fa fa-anchor',
+'fa fa-cube', 'fa fa-cube',
+'fa fa-leaf', 'fa fa-leaf',
+'fa fa-bomb', 'fa fa-bomb',
+'fa fa-bicycle', 'fa fa-bicycle'];
 
+const compareCards = array.icons;
 
-//Grabbing cards by CSS class, creating a new array each time after a card is clicked which holds no more than two cards clicked and then resets array to empty again
+// Method to shuffle cards for each new game. Function from http://stackoverflow.com/a/2450976
+Array.prototype.shuffle = function () {
+  var currentIndex = cardIcon.length, temporaryValue, randomIndex;
+
+  while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      cardIcon[currentIndex] = cardIcon[randomIndex];
+      cardIcon[randomIndex] = temporaryValue;
+  }
+
+  return shuffle;
+};
+}
+
+//Flip over cards
 const cardDeck = document.querySelectorAll('.card');
 let openCards = [];
 
@@ -21,25 +36,28 @@ cardDeck.forEach(function(card) {
       card.classList.add('open', 'show');
     });
 });
-//To start the game, prepare board
 
+//Check to see if the cards match
+if (card.classList = 'open', 'show') {
+  function compareCards() {
 
+  }
+} else {
 
-// Count the moves made to complete Game. After 8 moves, start removing stars.
-
-
-/* Shuffle cards for each new game. Function from http://stackoverflow.com/a/2450976
-function shuffle(cardIcon) {
-    var currentIndex = cardIcon.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        cardIcon[currentIndex] = cardIcon[randomIndex];
-        cardIcon[randomIndex] = temporaryValue;
-    }
-
-    return cardIcon;
 }
-*/
+)};
+
+//Start Game, call shuffle method to arrange new order of cards to the deck
+function newBoard() {
+  compareCards = 0;
+  const output = '';
+  icons.shuffle();
+    for var i =0; i < icons.length; i++) {
+      output += '<ul class="deck" + i +'" onclick="cardDeck(this,\''+iconArray[i]+'\')"></ul>';
+    }
+    document.getElementsByClassName('deck').innerHTML = output;
+}
+window.addEventListener(newBoard(););
+
+// Count the moves made to complete Game. After 16 moves, start removing stars.
+let count = 0;
